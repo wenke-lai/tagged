9bc2a31e155193db39c6e1d4ffe4bf9d3ed603a7
wenke.lai <wenke.studio@gmail.com>
2024-04-10 22:46:55 +0800

    feat(asset,tag): add relationships with user and tag

--- server/asset/models.py
+++ server/asset/models.py
@@ -1,5 +1,10 @@
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db import models
+
+from tag.models import Tag
+
+User = get_user_model()
 
 
 class Asset(models.Model):
@@ -11,6 +16,9 @@ class Asset(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.name
--- server/tag/models.py
+++ server/tag/models.py
@@ -1,10 +1,15 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+
+User = get_user_model()
 
 
 class Tag(models.Model):
     tag_id = models.BigAutoField(primary_key=True)
 
     name = models.CharField(max_length=100)
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
